48da636b31b7d299337821cc187d76ae4ff7b490
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-09-12 23:37:34 +0200

    More concise implem, relying directly on the aws java sdk

--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ scalaVersion := "2.10.6"
 crossScalaVersions := Seq("2.10.6", "2.11.8")
 
 libraryDependencies ++= Seq(
-  "com.github.seratch" %% "awscala" % "0.5.+",
+  "com.amazonaws" % "aws-java-sdk-s3" % "1.10.77",
   "com.lihaoyi" %% "utest" % "0.4.3" % "test"
 )
 
--- src/main/scala/coursier/cache/protocol/S3cHandler.scala
+++ src/main/scala/coursier/cache/protocol/S3cHandler.scala
@@ -1,82 +1,93 @@
 package coursier.cache.protocol
 
-import java.io.InputStream
+import java.io.ByteArrayInputStream
 import java.net._
 
-import awscala.s3._
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.ListObjectsRequest
 
-import scala.util.{Failure, Success, Try}
+import scala.collection.JavaConverters._
 
-/*
+/**
  * To avoid collision with `fm-sbt-s3-resolver` we added a different URL
  * format starting with `s3c` (S3 coursier).
  *
  * This way you can have your resolver URL with `s3c` and your publish URL with `s3`.
  *
  * Our handler only supports one kind of URL:
- * s3c://hostname[:port]/<bucket-name>
- *
- * For now the region in the url is being ignored.
- *
- * It does not support credentials in the URLs for security reasons.
- * You should provide them as environment variables or
- * in `.s3credentials` in $HOME, $HOME/.sbt, $HOME/.coursier
+ * s3c://<bucket-name>/<key-path>
  */
+class S3cHandler extends URLStreamHandlerFactory {
 
-class S3cHandler extends URLStreamHandler {
+  def createURLStreamHandler(protocol: String): URLStreamHandler =
+    if (protocol == "s3c")
+      new URLStreamHandler {
 
-  override def openConnection(url: URL): URLConnection = {
-    new URLConnection(url) {
-      override def getInputStream: InputStream = {
+        lazy val client = new AmazonS3Client
 
-        val endpoint = Option(url.getPort).filter(_ > 0).map(p => s"${url.getHost}:$p").getOrElse(url.getHost)
-        S3Client.setEndpoint(endpoint)
+        for (endpoint <- sys.props.get("aws.s3Endpoint"))
+          client.setEndpoint(endpoint)
 
-        val fullPath = url.getPath
-        val subPaths = fullPath.split("/").map(_.trim).filter(_.nonEmpty)
+        override def openConnection(url: URL) =
+          new URLConnection(url) {
 
-        subPaths.headOption.fold[InputStream] {
-          Console.err.println(s"Failed to get bucket name in (url:${url.toString})")
-          null
-        } { bucketName =>
-          // Bucket
-          val bucket = Bucket(bucketName)
+            override def getInputStream = {
 
-          // Key
-          val key = subPaths.tail.mkString("/")
+              val bucket = url.getHost
+              val path = url.getPath.stripPrefix("/")
 
-          Try {
-            if (fullPath.endsWith("/")) {
-              S3Client.prepareDirectoryList(bucket, s"$key/")
-            } else {
-              S3Client.prepareFileContents(bucket, key)
+              if (path.endsWith("/")) {
+
+                val entries = client
+                  .listObjects(
+                    // "/" as a delimiter to be returned only entries in the first level (no recursion),
+                    // with (pseudo) sub-directories indeed ending with a "/"
+                    new ListObjectsRequest(bucket, path, null, "/", null)
+                  )
+                  .getObjectSummaries
+                  .asScala
+                  .map { summary =>
+                    val key = summary.getKey
+                    assert(key.startsWith(path))
+
+                    val name = key.stripPrefix(path)
+
+                    // TODO escape characters?
+                    s"""<li><a href="$name">$name</a></li>"""
+                  }
+
+                if (entries.isEmpty)
+                  throw new NoSuchElementException(s"$url")
+                else {
+                  val page =
+                    s"""<!DOCTYPE html>
+                       |<html>
+                       |<head>
+                       |</head>
+                       |<body>
+                       |<ul>
+                       |${entries.mkString}
+                       |</ul>
+                       |</body>
+                       |</html>
+                     """.stripMargin
+
+                  val b = page.getBytes("UTF-8")
+                  new ByteArrayInputStream(b)
+                }
+              } else
+                client
+                  .getObject(bucket, path)
+                  .getObjectContent
             }
-          } match {
-            case Success(f) => f
-            case Failure(e) =>
-              Console.err.println(s"Failed to prepare (key:$key): ${e.getMessage}")
-              null
+
+            override def connect() = {
+              // initialize client if not already initialized
+              client
+            }
           }
-        }
-
       }
-
-      override def connect() {}
-
-    }
-  }
-
-}
-
-object S3cHandler {
-
-  def setupS3Handler() = URL.setURLStreamHandlerFactory(S3URLStreamHandlerFactory)
-
-  private object S3URLStreamHandlerFactory extends URLStreamHandlerFactory {
-    def createURLStreamHandler(protocol: String): URLStreamHandler = protocol match {
-      case "s3c" => new S3cHandler()
-      case _ => null
-    }
-  }
+    else
+      throw new IllegalArgumentException(s"protocol $protocol")
 
 }
